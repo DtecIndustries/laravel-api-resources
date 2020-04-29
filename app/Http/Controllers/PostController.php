a370ab2bbad40437ba0be657d4ee07761de81deb
Joachim Defour <joachim.defour@icloud.com>
2020-04-29 21:38:53 +0200

    Added Post model, controller & migration

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get posts
+        $posts = Post::paginate(15);
+        return $posts;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = $request->isMethod('put') ? Post::findOrFail($request->post_id) : new Post;
+
+        $post->id = $request->input('post_id');
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        if($post->save()){
+            return $post;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //get post
+        $post = Post::findOrFail($id);
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //get post
+        $post = Post::findOrFail($id);
+
+        if($post->delete()){
+            return $post;
+        }
+    }
+}
